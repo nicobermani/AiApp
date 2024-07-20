dc5468f982bd01b9ef8160c1a5b43da5233767cd
nicobermani <iamnicober2@gmail.com>
2024-07-20 11:22:05 +0530

    fix

---
+++ src/app/dev/@notes/page.js
@@ -0,0 +1,24 @@
+'use client'
+import React from 'react'
+
+const notes = ['^\\s*$\\n - Replace blank lines in Visual Studio', '^\\s*$\\n']
+
+const AiPlaygroundLinks = () => {
+  return (
+    <div className="p-4 max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+      <h2 className="text-2xl font-bold text-center mb-4">Notes</h2>
+      <ul className="divide-y divide-gray-200">
+        {notes.map((note, index) => (
+          <li key={index} className="flex items-center p-4">
+            <pre
+              className="whitespace-pre-wrap"
+              dangerouslySetInnerHTML={{ __html: note }}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default AiPlaygroundLinks
--- src/app/dev/layout.js
+++ src/app/dev/layout.js
@@ -1,10 +1,11 @@
-export default function AiLayout({ children, aiApps, links }) {
+export default function AiLayout({ children, aiApps, links,notes }) {
   return (
     <section>
       {children}
       <div className="min-h-screen flex">
         <div className=" ">{aiApps}</div>
         <div className="">{links}</div>
+        <div className="">{notes}</div>
       </div>
     </section>
   )
